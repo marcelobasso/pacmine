58cf6be4d41cdacd097e3640d056c4fad85035c6
bassoCelo <bassomarcelo.ba@gmail.com>
2023-08-01 02:07:21 -0300

    :sparkles: inclui carregamento e montagem do mapa

--- main.c
+++ main.c
@@ -1,64 +1,162 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Keyboard input
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2014-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+#define TAMANHO_MAPA 600
+#define MAX_PATH_SIZE 20
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+#define MAX_COLUNAS 30
+#define MAX_LINHAS 20
 
-    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+#define ALTURA_MAPA 600
+#define LARGURA_MAPA 900
+#define TAMANHO_TILE 30
+#define ALTURA_MENU 50
+#define SCREEN_WIDTH LARGURA_MAPA
+#define SCREEN_HEIGHT (ALTURA_MAPA + ALTURA_MENU)
+#define MAP_SIZE_HEIGHT (ALTURA_MAPA / TAMANHO_TILE)
+#define MAP_SIZE_WIDTH (LARGURA_MAPA / TAMANHO_TILE)
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+#define N_TOUPEIRAS 200
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
-        //----------------------------------------------------------------------------------
+// define elementos do mapa
+#define PAREDE_INDESTRUTIVEL '#'
+#define POS_INICIAL 'J'
+#define POS_INICIAL_TOUPEIRA 'T'
+#define OURO 'O'
+#define ESMERALDA 'E'
+#define AREA_SOTERRADA 'S'
+#define POWER_UP 'A'
+#define LIVRE ' '
 
-        // Draw
-        //----------------------------------------------------------------------------------
+typedef struct Pos {
+    int x, y;
+} POS;
+
+typedef struct Player {
+    POS pos;
+    int vidas;
+} PLAYER;
+
+typedef struct Toupeira {
+    POS pos;
+    int estado; // viva ou morta
+} TOUPEIRA;
+
+void desenhaMapa(int max_linhas, int max_colunas, PLAYER *player, TOUPEIRA *toupeiras, char mapa[MAX_LINHAS][MAX_COLUNAS]);
+void carregaMapa(char mapa[MAX_LINHAS][MAX_COLUNAS], int fase);
+
+int main() {
+    PLAYER player;
+    TOUPEIRA toupeiras[N_TOUPEIRAS];
+    char mapa[MAX_LINHAS][MAX_COLUNAS];
+
+    carregaMapa(mapa, 1);
+
+    InitWindow(LARGURA_MAPA, ALTURA_MAPA, "PACMINE - UFRGS");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
+            DrawText("PACMINE", 10, 10, 20, DARKGRAY);
 
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-            DrawCircleV(ballPosition, 50, MAROON);
+            // inicia mapa
+            desenhaMapa(MAX_LINHAS, MAX_COLUNAS, &player, toupeiras, mapa);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
+}
+
+void desenhaMapa(int max_linhas, int max_colunas, PLAYER *player, TOUPEIRA *toupeiras, char mapa[MAX_LINHAS][MAX_COLUNAS]) {
+    Color cor_bloco;
+    Rectangle bloco;
+    int l, c;
+    int toup_count = 0;
+
+    for (l = 0; l < MAX_LINHAS; l++) {
+        for (c = 0; c < MAX_COLUNAS; c++) {
+            cor_bloco = RAYWHITE;
+            toup_count = 0;
+
+            switch (mapa[l][c]) {
+                case PAREDE_INDESTRUTIVEL:
+                    cor_bloco = BLACK;
+                    break;
+
+                case POS_INICIAL:
+                    player->pos.x = c * TAMANHO_TILE;
+                    player->pos.y = l * TAMANHO_TILE;
+                    player->vidas = 3;
+
+                    cor_bloco = GREEN;
+                    break;
+
+                case POS_INICIAL_TOUPEIRA:
+                    toupeiras[toup_count].pos.x = c * TAMANHO_TILE;
+                    toupeiras[toup_count].pos.y = l * TAMANHO_TILE;
+                    toup_count++;
+
+                    cor_bloco = RED;
+                    break;
+
+                case OURO:
+                    cor_bloco = GOLD;
+                    break;
+
+                case ESMERALDA:
+                    cor_bloco = LIME;
+                    break;
+                
+                case AREA_SOTERRADA:
+                    cor_bloco = BROWN;
+                    break;
+                
+                case POWER_UP:
+                    cor_bloco = PINK;
+                    break;
+            }
+
+            bloco = (Rectangle) { 
+                x: c * TAMANHO_TILE, 
+                y: l * TAMANHO_TILE,
+                width: TAMANHO_TILE,
+                height: TAMANHO_TILE
+            };
+
+            DrawRectangleRec(bloco, cor_bloco);
+        }
+    }
+
+}
+
+void carregaMapa(char mapa[MAX_LINHAS][MAX_COLUNAS], int fase) {
+    char caminho_mapa[MAX_PATH_SIZE];
+    char linha_mapa[TAMANHO_MAPA];
+    FILE *arquivo_mapa;
+    int linha = 0;
+
+    sprintf(caminho_mapa, "./maps/mapa%d.txt", fase);
+    arquivo_mapa = fopen(caminho_mapa, "r");
+
+    if (NULL == arquivo_mapa) {
+        printf("file can't be opened \n");
+    }
+
+    while (!feof(arquivo_mapa)) {
+        fgets(linha_mapa, 32, arquivo_mapa); // ?
+        strcpy(mapa[linha], linha_mapa);
+        linha++;
+    }
+
+
+    fclose(arquivo_mapa);
 }
