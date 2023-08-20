d1e4d86c9cc9a44e02a65ebfee9d633577d91e79
bassoCelo <bassomarcelo.ba@gmail.com>
2023-08-20 00:06:27 -0300

    :fire: simplifica codigo e centraliza informacoes em struct jogo

--- main.c
+++ main.c
@@ -10,21 +10,14 @@
 #define MAX_COLUNAS 30
 #define MAX_LINHAS 20
 
-#define ALTURA_MAPA 600
-#define LARGURA_MAPA 900
 #define ALTURA_MENU 50
-#define SCREEN_WIDTH LARGURA_MAPA
-#define SCREEN_HEIGHT (ALTURA_MAPA + ALTURA_MENU)
-#define MAP_SIZE_HEIGHT (ALTURA_MAPA / ARESTA)
-#define MAP_SIZE_WIDTH (LARGURA_MAPA / ARESTA)
-
 #define MAX_TOUPEIRAS 200
-//***
+
 #define ARESTA 30
 #define PASSO 5
 #define PASSO_TOUPEIRAS 2
 #define QTD_VIDAS 3
-//***
+
 // define elementos do mapa
 #define PAREDE_INDESTRUTIVEL '#'
 #define POS_INICIAL 'J'
@@ -42,29 +35,29 @@
 #define SAIR 'S'
 #define VOLTAR 'V'
 
-typedef struct Pos {
-    int x, y;
-} POS;
-
 //Adicionei a struct Des para marcar o deslocamento
 
 typedef struct Player {
-    POS pos, posInicial, des;
+    Vector2 pos, posInicial, des;
     int vidas;
 } PLAYER;
 
 typedef struct Toupeira {
-    POS pos, posInicial, des;
+    Vector2 pos, posInicial, des;
     int estado; // viva ou morta
     int id;
 } TOUPEIRA;
 
 typedef struct {
     char mapa[PATH_MAX], mapas[10][PATH_MAX];
+    int altura_mapa, largura_mapa, aresta;
+    int qnt_toupeiras;
     char opcao;
+    double toup_muda_movimento;
 } JOGO;
 
-int dentroDosLimites(POS posicao, POS deslocamento, int lim_x, int lim_y) {
+// Função que verifica se a posicao atual + o deslocamento de um corpo ainda o manterá dentro dos limites da tela.
+int dentroDosLimites(Vector2 posicao, Vector2 deslocamento, int lim_x, int lim_y, int aresta) {
     int dentro = 1;
 
     if (posicao.x == 0 && deslocamento.x == -1) {
@@ -73,17 +66,19 @@ int dentroDosLimites(POS posicao, POS deslocamento, int lim_x, int lim_y) {
     else if (posicao.y == 0 && deslocamento.y == -1) {
         dentro = 0;
     }
-    else if (posicao.x == lim_x - ARESTA && deslocamento.x == 1){
+    else if (posicao.x == lim_x - aresta && deslocamento.x == 1){
         dentro = 0;
     }
-    else if (posicao.y == lim_y - ARESTA && deslocamento.y == 1) {
+    else if (posicao.y == lim_y - aresta && deslocamento.y == 1) {
         dentro = 0;
     }
 
     return dentro;
 }
 
-void move(POS *pos, POS des, int passo) {
+
+// Função responsável pela movimentação do jogador e dos inimigos.
+void move(Vector2 *pos, Vector2 des, int passo) {
     pos->x += des.x * passo;
     pos->y += des.y * passo;
 }
@@ -91,25 +86,25 @@ void move(POS *pos, POS des, int passo) {
 //*********************************************************************************
 //verifica se o player vai sair da tela
 //ainda vai ser melhoras para nao poder passar por obstaculos
-int podeMover(PLAYER player) {
-    return dentroDosLimites(player.pos, player.des, LARGURA_MAPA, ALTURA_MAPA);    
+int podeMover(PLAYER player, JOGO jogo) {
+    return dentroDosLimites(player.pos, player.des, jogo.largura_mapa, jogo.altura_mapa, ARESTA);    
 }
 
 //verifica se o inimigo vai sair dos limites da tela
 //ainda vai ser melhoras para nao poder passar por obstaculos
-int inimigoPodeMover(TOUPEIRA *toupeira, TOUPEIRA *toupeiras, int *qnt_toupeiras){
+int inimigoPodeMover(TOUPEIRA *toupeira, TOUPEIRA *toupeiras, JOGO jogo){
     int pode_mover = 1, rota_colisao = 0;
     int count = 0;
 
     // checa se a toupeira esta em rota de colisao com outra toupeira
-    for (count = 0; count < *qnt_toupeiras && !rota_colisao; count++) {
+    for (count = 0; count < jogo.qnt_toupeiras && !rota_colisao; count++) {
         if (toupeiras[count].id != toupeira->id) {
             rota_colisao = CheckCollisionRecs((Rectangle) { toupeira->pos.x + toupeira->des.x, toupeira->pos.y + toupeira->des.y, ARESTA, ARESTA}, (Rectangle) { toupeiras[count].pos.x, toupeiras[count].pos.y, ARESTA, ARESTA }); 
         }
     }
 
     // checa se a toupeira esta nos limites do mapa e nao esta em rota de colisao
-    pode_mover = dentroDosLimites(toupeira->pos, toupeira->des, LARGURA_MAPA, ALTURA_MAPA) && !rota_colisao; 
+    pode_mover = dentroDosLimites(toupeira->pos, toupeira->des, jogo.largura_mapa, jogo.altura_mapa, ARESTA) && !rota_colisao; 
 
     // inverte movimento caso nao puder se deslocar na direcao desejada
     if (!pode_mover) {
@@ -131,21 +126,29 @@ void resetPosicoes(PLAYER *player, TOUPEIRA *toupeiras, int qnt_toupeiras) {
     player->pos.y = player->posInicial.y;
 }
 
-void iniciaToupeiras(TOUPEIRA *toupeiras, char mapa[MAX_LINHAS][MAX_COLUNAS], int *qnt_toupeiras) {
-    int l, c;
+void iniciaToupeiras(TOUPEIRA *toupeiras, char mapa[MAX_LINHAS][MAX_COLUNAS], JOGO *jogo) {
+    int l, c, pos_vetor;
 
     for (l = 0; l < MAX_LINHAS; l++) {
         for (c = 0; c < MAX_COLUNAS; c++) {
+            pos_vetor = jogo->qnt_toupeiras;
+
             if (mapa[l][c] == 'T') {
-                toupeiras[*qnt_toupeiras].posInicial.x = c * ARESTA;
-                toupeiras[*qnt_toupeiras].posInicial.y = l * ARESTA;
-                toupeiras[*qnt_toupeiras].pos.x = c * ARESTA;
-                toupeiras[*qnt_toupeiras].pos.y = l * ARESTA;
-                toupeiras[*qnt_toupeiras].id = *qnt_toupeiras;
-                toupeiras[*qnt_toupeiras].des.x = 0;
-                toupeiras[*qnt_toupeiras].des.y = 0;
-                
-                *qnt_toupeiras += 1;
+                toupeiras[pos_vetor].posInicial.x = c * ARESTA;
+                toupeiras[pos_vetor].posInicial.y = l * ARESTA;
+                toupeiras[pos_vetor].pos.x = c * ARESTA;
+                toupeiras[pos_vetor].pos.y = l * ARESTA;
+                toupeiras[pos_vetor].id = pos_vetor;
+
+
+                toupeiras[pos_vetor].des.x = GetRandomValue(-1, 1);
+                if (toupeiras[pos_vetor].des.x == 0) {
+                    do {
+                        toupeiras[pos_vetor].des.y = GetRandomValue(-1, 1);
+                    } while (toupeiras[pos_vetor].des.y == 0);
+                }
+
+                jogo->qnt_toupeiras += 1;
             }
         }
 
@@ -222,8 +225,9 @@ void desenhaMapa(int max_linhas, int max_colunas, PLAYER *player, TOUPEIRA *toup
     }
 }
 
-void movimentaJogador(PLAYER *player) {
-    player->des = (POS) { x: 0, y: 0 };
+void movimentaJogador(PLAYER *player, int passo, TOUPEIRA *toupeiras, JOGO jogo) {
+    int i;
+    player->des = (Vector2) { 0, 0 };
 
     if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
         player->des.x = 1;
@@ -241,8 +245,52 @@ void movimentaJogador(PLAYER *player) {
         player->des.y = 1;
     }
 
-    if (podeMover(*player)) {
-       move(&player->pos, player->des, PASSO);
+    if (podeMover(*player, jogo)) {
+       move(&player->pos, player->des, passo);
+    }
+
+    // tira a vida do jogador caso ele encostar em uma toupeira
+    for (i = 0; i < jogo.qnt_toupeiras; i++) {
+        if (CheckCollisionRecs((Rectangle) { player->pos.x, player->pos.y, ARESTA, ARESTA }, (Rectangle) {toupeiras[i].pos.x, toupeiras[i].pos.y, ARESTA, ARESTA})) {
+            resetPosicoes(player, toupeiras, jogo.qnt_toupeiras);
+            player->vidas--;
+
+            if (player->vidas == 0) {
+
+            }
+        }
+    }
+}
+
+void movimentaToupeiras(TOUPEIRA *toupeiras, int passo_toupeiras, JOGO *jogo) {
+    int i;
+    double diff_time;
+
+    diff_time = GetTime() - jogo->toup_muda_movimento;
+
+    // movimento das toupeiras
+    // define uma direcao a cada segundo
+    if (diff_time >= 1) { // pq q funciona com 1?
+        for (i = 0; i < jogo->qnt_toupeiras; i++) {
+            // reseta o deslocamento
+            toupeiras[i].des = (Vector2) { 0, 0 };
+            toupeiras[i].des.x = GetRandomValue(-1, 1);
+
+            if (toupeiras[i].des.x == 0) {
+                do {
+                    toupeiras[i].des.y = GetRandomValue(-1, 1);
+                } while (toupeiras[i].des.y == 0);
+            }
+        }
+
+        jogo->toup_muda_movimento = GetTime();
+    }
+
+    // move as toupeiras
+    for (i = 0; i < jogo->qnt_toupeiras; i++) {
+        if (inimigoPodeMover(&toupeiras[i], toupeiras, *jogo)) {
+            move(&toupeiras[i].pos, toupeiras[i].des, PASSO_TOUPEIRAS);
+        }
     }
 }
 
@@ -269,19 +317,30 @@ void carregaMapa(char mapa[MAX_LINHAS][MAX_COLUNAS], int fase) {
     fclose(arquivo_mapa);
 }
 
+void iniciaJogo(JOGO *jogo) {
+    jogo->altura_mapa = MAX_LINHAS * ARESTA;
+    jogo->largura_mapa = MAX_COLUNAS * ARESTA;
+    jogo->qnt_toupeiras = 0;
+    jogo->toup_muda_movimento = GetTime();
+}
+
 int main() {
-    srand(time(NULL));
+    JOGO jogo;
     PLAYER player;
     TOUPEIRA toupeiras[MAX_TOUPEIRAS];
     char mapa[MAX_LINHAS][MAX_COLUNAS];
-    int i, cont, encerra = 0;
+    int encerra;
     int qnt_toupeiras = 0;
 
+    iniciaJogo(&jogo);
+
     carregaMapa(mapa, 1);
-    iniciaToupeiras(toupeiras, mapa, &qnt_toupeiras);
+    iniciaToupeiras(toupeiras, mapa, &jogo);
     iniciaJogador(&player, mapa);
 
-    InitWindow(LARGURA_MAPA, ALTURA_MAPA, "PACMINE - UFRGS");
+    qnt_toupeiras = jogo.qnt_toupeiras;
+
+    InitWindow(jogo.largura_mapa, jogo.altura_mapa, "PACMINE - UFRGS");
 
     SetTargetFPS(60);
 
@@ -291,50 +350,9 @@ int main() {
 
         ClearBackground(RAYWHITE);
 
-        //*********************************************************************************
         //movimento do jogador
-        movimentaJogador(&player);
-
-        // movimento das toupeiras
-        for (i = 0; i < qnt_toupeiras; i++) {
-            // define uma direcao a cada segundo
-            if (cont == 0) {
-                // reseta o deslocamento
-                toupeiras[i].des = (POS) {x: 0, y: 0};
-
-                toupeiras[i].des.x = GetRandomValue(-1, 1);
-                if (toupeiras[i].des.x == 0) {
-                    do {
-                        toupeiras[i].des.y = GetRandomValue(-1, 1);
-                    } while (toupeiras[i].des.y == 0);
-                }
-            }
-        }
-
-        cont++;
-        if (cont == 60) {
-            cont = 0;
-        }
-
-        // move as toupeiras
-        for (i = 0; i < qnt_toupeiras; i++) {
-            if (inimigoPodeMover(&toupeiras[i], toupeiras, &qnt_toupeiras)) {
-                move(&toupeiras[i].pos, toupeiras[i].des, PASSO_TOUPEIRAS);
-            }
-        }
-
-        //tira a vida do jogador caso ele encostar em uma toupeira
-        for (i = 0; i < qnt_toupeiras; i++) {
-            if (CheckCollisionRecs((Rectangle) { player.pos.x, player.pos.y, ARESTA, ARESTA }, (Rectangle) {toupeiras[i].pos.x, toupeiras[i].pos.y, ARESTA, ARESTA})) {
-                resetPosicoes(&player, toupeiras, qnt_toupeiras);
-                player.vidas--;
-
-                if (player.vidas == 0) {
-                    encerra = 1;
-                }
-            }
-        }
-        //*********************************************************************************
+        movimentaJogador(&player, PASSO, toupeiras, jogo);
+        movimentaToupeiras(toupeiras, PASSO_TOUPEIRAS, &jogo);
 
         // inicia mapa
         desenhaMapa(MAX_LINHAS, MAX_COLUNAS, &player, toupeiras, mapa, &qnt_toupeiras);
